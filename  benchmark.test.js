67eecddcfdce701aaa87a93d75a919c6008c3276
Somē Cho <itssomicho@gmail.com>
2023-05-23 10:14:07 +0200

    add wasm-flate compression and refactor benchmark and test

---  benchmark.test.js
+++  benchmark.test.js
@@ -1,84 +1,21 @@
-//COMPRESSION
-const LZString = require('./compression/LZString.js')
-const lzw = require('./compression/lzw.js')
-const ULZSS = require('./compression/ULZSS.js')
+const benchmark = require('./benchmark.js')
 
-// DATA
-const sketch = require('./data/sketch.js')
-const simplexNoise = require('./data/simplexNoise.js')
-const triangle = require('./data/triangle.js')
-
-const util = require('./compression/util.js')
+const tests = benchmark.algorithms.map(algorithm => {
+	return benchmark.data.map(d => {
+		return {
+			it: `ensures ${d.title} is reversible by ${algorithm.title}`,
+			compress: algorithm.encode,
+			decompress: algorithm.decode,
+			source: d.source
+		}
+	})
+}).flat()
 
 function reversible(compress, decompress, source) {
 	const encoded = compress(source)
 	const decoded = decompress(encoded)
 	expect(decoded).toBe(source)
 }
-
-const tests = [
-	{
-		it: "makes sure base64 compression of sketch is reversible",
-		compress: btoa,
-		decompress: atob,
-		source: sketch
-	}, {
-		it: "makes sure base64 compression of simplexNoise is reversible",
-		compress: btoa,
-		decompress: atob,
-		source: simplexNoise
-	}, {
-		it: "makes sure base64 compression of triangle is reversible",
-		compress: btoa,
-		decompress: atob,
-		source: triangle
-	}, {
-		it: "makes sure ULZSS compression of sketch is reversible",
-		compress: (s) => util.compress(s, ULZSS.encode),
-		decompress: (s) => util.decompress(s, ULZSS.decode),
-		source: sketch
-	}, {
-		it: "makes sure ULZSS compression of simplexNoise is reversible",
-		compress: (s) => util.compress(s, ULZSS.encode),
-		decompress: (s) => util.decompress(s, ULZSS.decode),
-		source: simplexNoise
-	}, {
-		it: "makes sure ULZSS compression of triangle is reversible",
-		compress: (s) => util.compress(s, ULZSS.encode),
-		decompress: (s) => util.decompress(s, ULZSS.decode),
-		source: triangle
-	}, {
-		it: "makes sure LZString compression of sketch is reversible",
-		compress: LZString.compressToBase64,
-		decompress: LZString.decompressFromBase64,
-		source: sketch
-	}, {
-		it: "makes sure LZString compression of simplexNoise is reversible",
-		compress: LZString.compressToBase64,
-		decompress: LZString.decompressFromBase64,
-		source: simplexNoise
-	}, {
-		it: "makes sure LZString compression of triangle is reversible",
-		compress: LZString.compressToBase64,
-		decompress: LZString.decompressFromBase64,
-		source: triangle
-	}, {
-		it: "makes sure LZW compression of sketch is reversible",
-		compress: (s)=>util.compress(s,lzw.encode),
-		decompress: (s)=>util.decompress(s,lzw.decode),
-		source: sketch
-	}, {
-		it: "makes sure LZW compression of simplexNoise is reversible",
-		compress: (s)=>util.compress(s,lzw.encode),
-		decompress: (s)=>util.decompress(s,lzw.decode),
-		source: simplexNoise
-	}, {
-		it: "makes sure LZW compression of triangle is reversible",
-		compress: (s)=>util.compress(s,lzw.encode),
-		decompress: (s)=>util.decompress(s,lzw.decode),
-		source: triangle
-	}
-]
 
 tests.forEach(t => {
 	it(t.it, () => reversible(t.compress, t.decompress, t.source))
--- benchmark.js
+++ benchmark.js
@@ -2,102 +2,86 @@
 const LZString = require('./compression/LZString.js')
 const lzw = require('./compression/lzw.js')
 const ULZSS = require('./compression/ULZSS.js')
+const flate = require('wasm-flate')
+const util = require('./compression/util.js')
 
 // DATA
 const sketch = require('./data/sketch.js')
 const simplexNoise = require('./data/simplexNoise.js')
 const triangle = require('./data/triangle.js')
+const modele = require('./data/modele.js')
+const quil = require('./data/quil.js')
+const data = [
+	{ title: "sketch", source: sketch },
+	{ title: "simplexNoise", source: simplexNoise },
+	{ title: "triangle", source: triangle },
+	{ title: "modele", source: modele },
+	{ title: "quil", source: quil },
+]
 
-const util = require('./compression/util.js')
+const algorithms = [
+	{
+		title: "no compression",
+		encode: (s) => s,
+		decode: (s) => s
+	}, {
+		title: "base 64",
+		encode: btoa,
+		decode: atob
+	}, {
+		title: "u-lzss",
+		encode: (s) => util.compress(s, ULZSS.encode),
+		decode: (s) => util.decompress(s, ULZSS.decode)
+	}, {
+		title: "lz-string",
+		encode: LZString.compressToBase64,
+		decode: LZString.decompressFromBase64
+	}, {
+		title: "lzw",
+		encode: (s) => util.compress(s, lzw.encode),
+		decode: (s) => util.decompress(s, lzw.decode)
+	}, {
+		title: "gzip",
+		encode: flate.gzip_encode,
+		decode: flate.gzip_decode
+	}, {
+		title: "zlib",
+		encode: flate.zlib_encode,
+		decode: flate.zlib_decode
+	}, {
+		title: "deflate",
+		encode: flate.deflate_encode,
+		decode: flate.deflate_decode,
+	},
+]
+
+const benchmarks = algorithms.map(algorithm => {
+	return data.map(d => {
+		return {
+			title: d.title,
+			source: d.source,
+			compressionMethod: algorithm.title,
+			compressFn: algorithm.encode
+		}
+	})
+}).flat()
 
 function benchmark(opts) {
 	const length = opts.source.length;
 	const encoded = opts.compressFn(opts.source);
 	const encodedLength = encoded.length;
 	const ratio = Math.floor(encodedLength / length * 10000.0) / 100.0;
-
 	console.log(`${opts.title} - ${opts.compressionMethod}`)
-	console.log(`${opts.title} uncompressed length: ${length} chars`)
 	console.log(`Compressed length: ${encodedLength} chars`)
 	console.log(`Compression ratio: ${ratio}%\n`)
-
 	return encodedLength;
 }
-
-const benchmarks = [
-	{
-		title: "Sketch",
-		compressionMethod: "no compression",
-		source: sketch,
-		compressFn: btoa
-	},
-	{
-		title: "Simplex Noise",
-		compressionMethod: "no compression",
-		source: simplexNoise,
-		compressFn: btoa
-	},
-	{
-		title: "Triangle.js",
-		compressionMethod: "no compression",
-		source: triangle,
-		compressFn: btoa
-	},
-	{
-		title: "Sketch",
-		compressionMethod: "ULZSS compression",
-		source: sketch,
-		compressFn: (s) => util.compress(s, ULZSS.encode)
-	},
-	{
-		title: "Simplex Noise",
-		compressionMethod: "ULZSS compression",
-		source: simplexNoise,
-		compressFn: (s) => util.compress(s, ULZSS.encode)
-	},
-	{
-		title: "Triangle.js",
-		compressionMethod: "ULZSS compression",
-		source: triangle,
-		compressFn: (s) => util.compress(s, ULZSS.encode)
-	},
-	{
-		title: "Sketch",
-		compressionMethod: "LZString compression",
-		source: sketch,
-		compressFn: LZString.compressToBase64
-	},
-	{
-		title: "Simplex Noise",
-		compressionMethod: "LZString compression",
-		source: simplexNoise,
-		compressFn: LZString.compressToBase64
-	},
-	{
-		title: "Triangle.js",
-		compressionMethod: "LZString compression",
-		source: triangle,
-		compressFn: LZString.compressToBase64
-	}, {
-		title: "Sketch",
-		compressionMethod: "LZW compression",
-		source: sketch,
-		compressFn: (s) => util.compress(s, lzw.encode)
-	},
-	{
-		title: "Simplex Noise",
-		compressionMethod: "LZW compression",
-		source: simplexNoise,
-		compressFn: (s) => util.compress(s, lzw.encode)
-	},
-	{
-		title: "Triangle.js",
-		compressionMethod: "LZW compression",
-		source: triangle,
-		compressFn: (s) => util.compress(s, lzw.encode)
-	},
-]
 
 benchmarks.forEach(b => {
 	benchmark(b)
 })
+
+module.exports = {
+	data,
+	algorithms
+}
